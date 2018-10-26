453d73190da8508f56d7d65e2d1f0abd603e36f9
Sankarsreekumar <44466674+Sankarsreekumar@users.noreply.github.com>
2018-10-26 10:38:47 +0530

    Update display.java

--- display.java
+++ display.java
@@ -3,6 +3,6 @@ class Print
 public static void main(String[] args)
 {
 System.out.println("Hello world");
-  System.out.println("This is branch 1");
+  System.out.println("This is branch one");
 }
 }
